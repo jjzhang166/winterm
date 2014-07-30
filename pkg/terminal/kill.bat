7aab24326850a6d86ad6a78d0fd1c73b3151b5b9
zhangbo <zhangbo7364@163.com>
2014-07-30 14:10:22 +0800

    安装打包程序（使用SetupFactory7.0打包）

---
+++ pkg/terminal/kill.bat
@@ -0,0 +1,6 @@
+taskkill /f /im update.exe /t 
+taskkill /f /im service.exe /t 
+taskkill /f /im telnetd.exe /t 
+taskkill /f /im terminal.exe /t 
+taskkill /f /im shell.exe /t 
+taskkill /f /im registermach.exe /t 
---
+++ pkg/terminal/log/run.bat
@@ -0,0 +1,2 @@
+logview *.log
+pause
